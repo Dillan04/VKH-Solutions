354c87ec76563a9eff7af68463ed1a82ca48271e
Dillan04 <dillanhammond7@gmail.com>
2025-01-06 03:22:01 +0100

    2nd Commit

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ document.querySelectorAll('.nav-link').forEach(link => {
         setTimeout(() => link.classList.remove('clicked'), 1000);
     });
 });
+
+// Select menu toggle button and navbar links
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+// Toggle active class on click
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
